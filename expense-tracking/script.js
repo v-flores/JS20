2a8c67bb4e1d7e0f89a34a24a19595e2565591cc
v-flores <44541135+v-flores@users.noreply.github.com>
2020-02-01 00:24:27 -0600

    typos and transactionsDOM fix

--- expense-tracking/script.js
+++ expense-tracking/script.js
@@ -1,23 +1,59 @@
 const balance = document.getElementById('balance');
-const money_plus = document.getElementById('money_plus');
-const money_minus = document.getElementById('money_minus');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
 const list = document.getElementById('list');
 const form = document.getElementById('form');
 const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
-const dummyTransactions = [
-  { id: 1, text: 'Flower', amount: -20},
-  { id: 2, text: 'Salary', amount: 300},
-  { id: 3, text: 'Book', amount: -10},
-  { id: 4, text: 'Camera', amount: 150},
-];
+// const dummyTransactions = [
+//   { id: 1, text: 'Flower', amount: -20 },
+//   { id: 2, text: 'Salary', amount: 300 },
+//   { id: 3, text: 'Book', amount: -10 },
+//   { id: 4, text: 'Camera', amount: 150 }
+// ];
 
-let transactions = dummyTransactions;
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem('transactions')
+);
 
+let transactions =
+  localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+
+// Add transaction
+function addTransaction(e) {
+  e.preventDefault();
+
+  if (text.value.trim() === '' || amount.value.trim() === '') {
+    alert('Please add a text and amount');
+  } else {
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: +amount.value
+    };
+
+    transactions.push(transaction);
+
+    addTransactionDOM(transaction);
+
+    updateValues();
+
+    updateLocalStorage();
+
+    text.value = '';
+    amount.value = '';
+  }
+}
+
+// Generate random ID
+function generateID() {
+  return Math.floor(Math.random() * 100000000);
+}
 
 // Add transactions to DOM list
 function addTransactionDOM(transaction) {
+  // Get sign
   const sign = transaction.amount < 0 ? '-' : '+';
 
   const item = document.createElement('li');
@@ -27,41 +63,51 @@ function addTransactionDOM(transaction) {
 
   item.innerHTML = `
     ${transaction.text} <span>${sign}${Math.abs(
-      transaction.amount
-    )}</span> <button class="delete-btn">x</button>
+    transaction.amount
+  )}</span> <button class="delete-btn" onclick="removeTransaction(${
+    transaction.id
+  })">x</button>
   `;
 
   list.appendChild(item);
 }
 
-
-// Update balance, income, & expense
+// Update the balance, income and expense
 function updateValues() {
-  const amounts = transactions.map(transaction =>
-    transaction.amount);
+  const amounts = transactions.map(transaction => transaction.amount);
 
-    const total = amounts.reduce((acc, item) => (acc += item), 0)
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
     .toFixed(2);
 
-    const income = amounts
-      .filter(item => item > 0)
-      .reduce((acc, item) => (acc += item), 0)
-      .toFixed(2);
+  const expense = (
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
 
-    const expense = (
-      amounts.filter(item => item < 0).reduce((acc, item) => (acc +=
-      item), 0) *
-      -1
-    ).toFixed(2);
-
-    balance.innerText = `$${total}`;
-    money_plus.innerText = `$${income}`;
-    money_minus.innerText = `$${expense}`;
+  balance.innerText = `$${total}`;
+  money_plus.innerText = `$${income}`;
+  money_minus.innerText = `$${expense}`;
 }
 
+// Remove transaction by ID
+function removeTransaction(id) {
+  transactions = transactions.filter(transaction => transaction.id !== id);
 
+  updateLocalStorage();
 
-// Initi App
+  init();
+}
+
+// Update local storage transactions
+function updateLocalStorage() {
+  localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
+// Init app
 function init() {
   list.innerHTML = '';
 
@@ -71,3 +117,4 @@ function init() {
 
 init();
 
+form.addEventListener('submit', addTransaction);
